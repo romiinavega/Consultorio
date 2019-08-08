3da211ab33d8bf139694efa11d22aacee39dba0f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-08 07:42:29 -0700

    Agregar archivos de proyecto.

---
+++ Consultorio1/Consulta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consultorio1
+{
+    class Consulta
+    {
+        DateTime fecha;
+        Paciente paciente;
+        string sintomas;
+        string diagnostico;
+        string tratamiento;
+
+    }
+}
---
+++ Consultorio1/Paciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consultorio1
+{
+    class Paciente
+    {
+        //Atributos (variables)
+        string nombre;
+        string direccion;
+        string telefono;
+        DateTime fechaDeNacimiento;
+        string enfermedadesCronicas;
+
+        //Metodos (funciones)
+
+    }
+}
